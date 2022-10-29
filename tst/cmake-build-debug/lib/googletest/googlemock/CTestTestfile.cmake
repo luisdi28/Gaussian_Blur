066b40169c7f4ac1f53d59c7894f315ec6cf384d
luisdi28 <luisdi.28@estudiantec.cr>
2022-10-29 12:38:28 -0600

    Commit pruebas unitarias

---
+++ tst/cmake-build-debug/lib/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/luis/CLionProjects/Proyecto 1 - Datos II/Gaussian_Blur/tst/lib/googletest/googlemock
+# Build directory: /home/luis/CLionProjects/Proyecto 1 - Datos II/Gaussian_Blur/tst/cmake-build-debug/lib/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
