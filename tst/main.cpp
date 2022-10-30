d280b11046d7ac2eebe5fdbb520ee38053d0f7e1
luisdi28 <luisdi.28@estudiantec.cr>
2022-10-29 19:25:32 -0600

    Commit pruebas unitarias y comentarios

--- tst/main.cpp
+++ tst/main.cpp
@@ -10,52 +10,55 @@ class Server{
 public:
 
     int método() {
-        // Read the image file
+        // Lee la ruta de la imagen
         Mat image = imread("/home/luis/CLionProjects/Proyecto 1 - Datos II/Gaussian_Blur/tst/cuadras.jpg");
 
-        // Check for failure
+        // Chequea si hay fallos
         if (image.empty()) {
             cout << "Could not open or find the image" << endl;
             cin.get(); //wait for any key press
             return -1;
         }
 
-        //Blur the image with 3x3 Gaussian kernel
+        //Hace la imagen con borrosa con el nucleo de Gaussian 3x3
         Mat image_blurred_with_3x3_kernel;
         GaussianBlur(image, image_blurred_with_3x3_kernel, Size(3, 3), 0);
 
-        //Blur the image with 5x5 Gaussian kernel
+        //Hace la imagen con borrosa con el nucleo de Gaussian 5x5
         Mat image_blurred_with_5x5_kernel;
         GaussianBlur(image, image_blurred_with_5x5_kernel, Size(5, 5), 0);
 
-        //Define names of the windows
+        // Define el nombre de las ventanas emergentes
         String window_name = "Original";
         String window_name_blurred_with_3x3_kernel = "Lotus Blurred with 3 x 3 Gaussian Kernel";
         String window_name_blurred_with_5x5_kernel = "Lotus Blurred with 5 x 5 Gaussian Kernel";
 
-        // Create windows with above names
+        // Crea ventanas con los nombres de arriba
         namedWindow(window_name);
         namedWindow(window_name_blurred_with_3x3_kernel);
         namedWindow(window_name_blurred_with_5x5_kernel);
 
-        // Show our images inside the created windows.
+        // Muestra las imagenes dentro de las ventanas creadas
         imshow(window_name, image);
         imshow(window_name_blurred_with_3x3_kernel, image_blurred_with_3x3_kernel);
         imshow(window_name_blurred_with_5x5_kernel, image_blurred_with_5x5_kernel);
 
-        waitKey(0); // Wait for any keystroke in the window
+        waitKey(0); // Espera por alguna pulsación de teclas en la ventana
 
-        destroyAllWindows(); //destroy all opened windows
+        destroyAllWindows(); // Destruye todas las ventanas creadas
 
         return 0;
     }
 };
 
+//Creación de la prueba unitaria
 TEST(SeverTEST, Test1){
     Server servidor;
 
     EXPECT_EQ(servidor.método(), 0);
 }
+
+//Main que corre dicha prueba unitaria
 
 int main(int argc, char **argv){
     ::testing::InitGoogleTest(&argc, argv);
